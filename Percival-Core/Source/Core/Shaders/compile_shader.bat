047f9c332fbaae7514bca42703d8cf42731cb823
Hadrien Belleville <hadrien.belleville22@gmail.com>
2024-08-02 17:58:55 +0200

    :sparkles: shader compilation

--- Percival-Core/Source/Core/Shaders/compile_shader.bat
+++ Percival-Core/Source/Core/Shaders/compile_shader.bat
@@ -1,3 +1,5 @@
-C:/VulkanSDK/1.3.283.0/Bin/glslc.exe simple-shader.vert -o vert.spv
-C:/VulkanSDK/1.3.283.0/Bin/glslc.exe simple-shader.frag -o frag.spv
+set SHADER_DIR=%~dp0
+
+C:/VulkanSDK/1.3.283.0/Bin/glslc.exe "%SHADER_DIR%simple_shader.vert" -o "%SHADER_DIR%vert.spv"
+C:/VulkanSDK/1.3.283.0/Bin/glslc.exe "%SHADER_DIR%simple_shader.frag" -o "%SHADER_DIR%frag.spv"
 pause
